43673104b1147a3e8f1d9e7f6d5d190c862e72de
TommyV2 <coconutv123@gmail.com>
2023-03-28 09:22:55 +0200

    added back flags in composer

---
+++ time_test.py
@@ -0,0 +1,52 @@
+import time
+import requests
+
+
+apps = {
+    'laravel_php': '8000',
+    'kotlin_ktor': '8001',
+    # 'julia_genie': '8002',
+    'js_express': '8003',
+    'python_fastapi': '8004',
+    'java_spring': '8006',
+}
+
+endpoints = [('response', 'GET'), ('database_read', 'GET'), ('database_write', 'POST')]
+
+def run_test_get(url, retry_number):
+    avg_time = 0
+    for i in range(retry_number):
+        start = time.time()
+        response = requests.get(url)
+        end = time.time()
+        avg_time += (end-start)
+    return avg_time/retry_number
+
+def run_test_post(url, retry_number):
+    avg_time = 0
+    for i in range(retry_number):
+        start = time.time()
+        response = requests.post(url)
+        end = time.time()
+        avg_time += (end-start)
+    return avg_time/retry_number
+
+def append_to_file(text):
+    with open("results.txt", "a") as file1:
+        file1.write(f"{text}\n")
+
+retry_number = 100
+
+for key in apps:
+    port = apps[key]
+    for endpoint in endpoints:
+        action_name = endpoint[0]
+        action_type = endpoint[1]
+        url = f'http://localhost:{port}/{action_name}'
+        if action_type == 'GET':
+            average_time = run_test_get(url, retry_number)
+        else:
+            average_time = run_test_post(url, retry_number)
+        
+        append_to_file(f'{key}: {action_name} time = {average_time}')
+
