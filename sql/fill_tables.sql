965144bbf0bb78e43552c8a08c76f65513e68f35
Jakub Słowikowski <45019594+KubaSlowikowski@users.noreply.github.com>
2023-03-29 17:42:19 +0200

    SQL initial data (#10)
    * SQL initial data
    
    * changed static values to loop
    
    * updated results
    
    ---------
    
    Co-authored-by: Z6FFK <ffk@kmd.dk>
    Co-authored-by: TommyV2 <coconutv123@gmail.com>

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -1,7 +1,7 @@
-INSERT INTO product(product_name, product_price)
-VALUES
-    ('TV', 1500),
-    ('Car', 32000),
-    ('Watch', 400),
-    ('Bed', 870);
-
+do $$
+begin
+for r in 1..50000 loop
+insert into product(product_name, product_price) values('Product' || r, 20 + floor(random() * 500)::int);
+end loop;
+end;
+$$;
