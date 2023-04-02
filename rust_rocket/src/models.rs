7497844d0a322f9820754d2f32ec36b921f8636e
TommyV2 <coconutv123@gmail.com>
2023-04-02 15:21:05 +0200

    added rust rocker

---
+++ rust_rocket/src/models.rs
@@ -0,0 +1,29 @@
+use rocket::serde::{Deserialize, Serialize};
+
+use diesel::{AsChangeset, Insertable, Queryable};
+
+use crate::schema::product;
+
+#[derive(Serialize, Queryable, Debug)]
+#[serde(crate = "rocket::serde")]
+pub struct Artist {
+    pub product_id: i32,
+    pub product_name: String,
+    pub product_price: i32,
+}
+
+#[derive(Deserialize, Insertable, Debug)]
+#[serde(crate = "rocket::serde")]
+#[table_name = "product"]
+pub struct NewArtist {
+    pub product_name: String,
+    pub product_price: i32,
+}
+
+#[derive(Deserialize, AsChangeset, Debug)]
+#[serde(crate = "rocket::serde")]
+#[table_name = "product"]
+pub struct UpdatedArtist {
+    pub product_name: Option<String>,
+    pub product_price: Option<i32>,
+}
---
+++ rust_rocket/src/schema.rs
@@ -0,0 +1,7 @@
+table! {
+    product (product_id) {
+        product_id -> Int4,
+        product_name -> Varchar,
+        product_price -> Int4,
+    }
+}
