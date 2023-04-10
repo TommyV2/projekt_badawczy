b643b6cfd197675a81f2bbfa815e443e35c11a0b
AnterroZZ <anterroz@gmail.com>
2023-04-10 22:52:16 +0200

    Added database routes

--- julia_genie/routes.jl
+++ julia_genie/routes.jl
@@ -1,13 +1,19 @@
-using Genie.Router
+using Genie.Router, Genie.Responses
+using SearchLight
 
 route("/response") do
   "Hello world"
 end
 
 route("/database_read") do
-  "Route for database read"
+  query = "SELECT * FROM product"
+  cur = SearchLight.query(query)
+  return cur
 end
 
-route("/database_write") do
-  "Route for database write"
+route("/database_write", method = POST) do
+  query = "INSERT INTO product (product_name, product_price) VALUES ('Chair', '203' )"
+  SearchLight.query(query)
+  return setstatus(201)
 end
+
