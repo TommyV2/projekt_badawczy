dc47a4118052457dbed7c3d6637dface02befca8
AnterroZZ <anterroz@gmail.com>
2023-04-11 17:36:37 +0200

    converting to json on response added

--- julia_genie/routes.jl
+++ julia_genie/routes.jl
@@ -1,5 +1,18 @@
 using Genie.Router, Genie.Responses
 using SearchLight
+import Genie.Renderer.Json: json
+
+function dataframe_to_json(df)
+  li = []
+  for row in eachrow(df)
+      di = Dict()
+      for name in names(row)
+          di[string(name)] = row[name]
+      end
+      push!(li, di)
+  end
+  li
+end
 
 route("/response") do
   "Hello world"
@@ -8,7 +21,8 @@ end
 route("/database_read") do
   query = "SELECT * FROM product"
   cur = SearchLight.query(query)
-  return cur
+
+  dataframe_to_json(cur) |> json
 end
 
 route("/database_write", method = POST) do
