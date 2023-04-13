ea121f024d26b15a7b0a8df39f15a6083a372b06
Jakub Słowikowski <45019594+KubaSlowikowski@users.noreply.github.com>
2023-04-13 16:10:25 +0200

    Ruby on rails (#14)
    * Added initial ROR project.
    
    * Added initial ROR project.
    
    * Configured database.
    
    * Roby on Rails Docker
    
    * Changed ROR port from 3000 to 3001
    
    ---------
    
    Co-authored-by: Z6FFK <ffk@kmd.dk>

---
+++ ruby_on_rails/app/controllers/database_controller.rb
@@ -0,0 +1,17 @@
+class DatabaseController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:write]
+
+  def read
+    @products = Product.select(:product_id, :product_name, :product_price)
+    render json:@products
+  end
+
+  def write
+    @product = Product.new(product_params)
+    @product.save
+  end
+
+  def product_params
+    params.permit(:product_id, :product_name, :product_price)
+  end
+end
