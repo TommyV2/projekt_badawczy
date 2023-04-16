c7c3a8b71b067ab6112c13ef5d470e070e1950f7
Andrzej Smoliński <58042259+AnterroZZ@users.noreply.github.com>
2023-04-16 17:46:12 +0200

    modified for orm approach (#20)

--- sql/fill_tables.sql
+++ sql/fill_tables.sql
@@ -2,6 +2,7 @@ do $$
 begin
 for r in 1..10 loop
 insert into product(product_name, product_price) values('Product' || r, 20 + floor(random() * 500)::int);
+insert into products(product_name, product_price) values('Product' || r, 20 + floor(random() * 500)::int);
 end loop;
 end;
 $$;
