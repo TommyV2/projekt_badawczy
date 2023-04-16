c8f4c16cbc2f14f1fc5857e0401caa0a3f58acec
TommyV2 <58606334+TommyV2@users.noreply.github.com>
2023-04-16 17:47:26 +0200

    fix sql scripts

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -11,10 +11,3 @@ CREATE TABLE IF NOT EXISTS product (
   product_price int,
   PRIMARY KEY (product_id)
 );
-
-CREATE TABLE IF NOT EXISTS products (
-  id SERIAL NOT NULL,
-  product_name varchar(250) NOT NULL,
-  product_price int,
-  PRIMARY KEY (id)
-);
