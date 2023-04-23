43d29b2affb6369abf3b860e48a28831f56dfcf8
przybjul <przybjul@gmail.com>
2023-04-23 21:27:42 +0200

    endpoint many & conns

--- aspnetapp/app/Controllers/ValuesController.cs
+++ aspnetapp/app/Controllers/ValuesController.cs
@@ -38,6 +38,32 @@ namespace aspnetapp.Controllers
                 return resultStrings;
             }
         }
+        [HttpGet("database_read_conn")]
+        public IEnumerable<string> GetProductPrice2()
+        {
+            string connectionString = "Host=db;Username=docker;Password=docker;Database=exampledb";
+            List<string> results = new List<string>();
+            
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("SELECT * FROM product", connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(0);
+                        string name = reader.GetString(1);
+                        int price = reader.GetInt32(2);
+                        string result = $"{id}: {name} - {price}";
+                        results.Add(result);
+                    }
+                }
+            }
+            
+            return results;
+        }
         // GET: /values/product/{product_id}
         [HttpGet("product/{product_id}")]
         public IActionResult GetProductById(int product_id)
@@ -59,7 +85,7 @@ namespace aspnetapp.Controllers
         }
         // POST: /values/database_write
         [HttpPost("database_write")]
-        public IActionResult AddRecord()
+        public IActionResult AddRecord1()
         {
             using (var connection = new NpgsqlConnection(connectionString))
             {
@@ -77,8 +103,86 @@ namespace aspnetapp.Controllers
                 }
             }
         }
-    }
+        [HttpPost("database_write_conn")]
+        public IActionResult AddRecord2()
+        {
+            string connectionString = "Host=db;Username=docker;Password=docker;Database=exampledb";
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
 
+                using (var command = new NpgsqlCommand("INSERT INTO product (product_name, product_price) VALUES ('Chair', '203')", connection))
+                {
+                    command.ExecuteNonQuery();
+                    return CreatedAtAction(nameof(GetProductPrice), null);
+                }
+            }
+        }
+        [HttpPost("database_write_many")]
+        public IActionResult AddRecords3()
+        {
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var products = new List<Product>
+                {
+                    new Product { product_name = "Chair", product_price = 203 },
+                    new Product { product_name = "Table", product_price = 450 },
+                    new Product { product_name = "Lamp", product_price = 85 },
+                    new Product { product_name = "Sofa", product_price = 750 },
+                    new Product { product_name = "Desk", product_price = 325 },
+                    new Product { product_name = "Bookcase", product_price = 280 },
+                    new Product { product_name = "TV Stand", product_price = 195 },
+                    new Product { product_name = "Mirror", product_price = 110 },
+                    new Product { product_name = "Cabinet", product_price = 375 },
+                    new Product { product_name = "Clock", product_price = 70 }
+                };
+
+                var values = new List<string>();
+
+                foreach (var product in products)
+                {
+                    values.Add($"('{product.product_name}', {product.product_price})");
+                }
+
+                var valuesString = string.Join(", ", values);
+
+                var result = connection.Execute($"INSERT INTO product (product_name, product_price) VALUES {valuesString}");
+
+                if (result == products.Count)
+                {
+                    return CreatedAtAction(nameof(GetProductPrice), null);
+                }
+                else
+                {
+                    return BadRequest("Failed to insert records into the database.");
+                }
+            }
+        }
+        [HttpPost("database_write_many_conn")]
+        public IActionResult AddRecords4()
+        {
+            string connectionString = "Host=db;Username=docker;Password=docker;Database=exampledb";
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new NpgsqlCommand("INSERT INTO product (product_name, product_price) VALUES (@product_name, @product_price)", connection))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        command.Parameters.AddWithValue("product_name", "Chair");
+                        command.Parameters.AddWithValue("product_price", (203+i));
+                        command.ExecuteNonQuery();
+                        command.Parameters.Clear();
+                    }
+                    return CreatedAtAction(nameof(GetProductPrice), null);
+                }
+            }
+        }
+    }
+    
     public class Product
     {
         public int product_id { get; set; }
--- python_fastapi/app/main.py
+++ python_fastapi/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String, Sequence, DDL, event
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg2
 
 app = FastAPI()
 
@@ -29,6 +30,24 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def get_response():
     return {"Hello": "World"}
 
+@app.get("/database_read_conn")
+async def get_product_price():
+    # Connect to our database
+    conn = psycopg2.connect(
+        database="exampledb",
+        user="docker",
+        password="docker",
+        host="db",
+        port=5432,
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM product;")
+    result = cur.fetchall()
+    print(f"{result=}")
+    cur.close()
+    conn.close()
+    return result
+
 @app.get("/database_read")
 async def get_product_price():
     # Retrieve all products from the database
@@ -45,13 +64,65 @@ async def get_product_price(product_id: int):
     db.close()
     return product
 
+@app.post("/database_write_conn", status_code=201)
+async def add_record():
+    print("Create endpoint hit!!")
+
+    conn = psycopg2.connect(
+        database="exampledb",
+        user="docker",
+        password="docker",
+        host="db",
+        port="5432",
+    )
+    cur = conn.cursor()
+    cur.execute(
+        f"INSERT INTO product (product_name, product_price) VALUES ('ABC', '203' )"
+    )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
 @app.post("/database_write", status_code=201)
 async def add_record():
     print("Create endpoint hit!!")
 
     # Add a new product record to the database
     db = SessionLocal()
-    new_product = Product(product_name='Chair', product_price=203)
+    new_product = Product(product_name='ABC', product_price=203)
     db.add(new_product)
+    db.commit()
+    db.close()
+
+@app.post("/database_write_many_conn", status_code=201)
+async def add_record():
+    print("Create endpoint hit!!")
+
+    conn = psycopg2.connect(
+        database="exampledb",
+        user="docker",
+        password="docker",
+        host="db",
+        port="5432",
+    )
+    cur = conn.cursor()
+    for insert in range (0, 10):
+        cur.execute(
+            f"INSERT INTO product (product_name, product_price) VALUES ('ABC{insert}', '20{insert}' )"
+        )
+    conn.commit()
+    cur.close()
+    conn.close()
+
+
+@app.post("/database_write_many", status_code=201)
+async def add_record():
+    print("Create endpoint hit!!")
+    # Add a new product record to the database
+    db = SessionLocal()
+    for insert in range (10, 10):
+        new_product = Product(product_name=f'ABC{insert}', product_price=(200 + insert))
+        db.add(new_product)
     db.commit()
     db.close()
