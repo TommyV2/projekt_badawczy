9dc4abab680d1cb2da92c3fb1ded2bec4a88b9fb
czarkom <mczarkowski@onet.pl>
2023-04-24 19:01:09 +0200

    Add full endpoint set with connectors and ORM

--- go_gin/go.mod
+++ go_gin/go.mod
@@ -14,7 +14,8 @@ require (
 	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.0 // indirect
+	github.com/jackc/pgx/v5 v5.3.1 // indirect
+	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
@@ -31,6 +32,7 @@ require (
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
--- go_gin/main.go
+++ go_gin/main.go
@@ -8,7 +8,10 @@ import (
   "badawczy/database"
   "strconv"
   "badawczy/model"
+  "github.com/jackc/pgx/v5"
   "fmt"
+  "os"
+  ctx "context"
 )
 
 func main() {
@@ -23,6 +26,8 @@ func loadEnv() {
         log.Fatal("Error loading .env file")
     }
 }
+
+// var conn *pgx.Conn
 
 func loadDatabase() {
     database.Connect()
@@ -40,6 +45,21 @@ func serveApplication() {
     context.JSON(http.StatusOK, database.GetAllProducts())    
   })
 
+  router.GET("/database_read_conn", func(context *gin.Context) {
+    conn, _ := pgx.Connect(ctx.Background(), os.Getenv("DB_URL"))
+	  defer conn.Close(ctx.Background())
+	  rows, _ := conn.Query(ctx.Background(), "select * from product")
+    defer rows.Close()
+    var products []model.Product
+    for rows.Next() {
+      product := model.Product{}
+      rows.Scan(&product.Id, &product.Name, &product.Price)
+      products = append(products, product)
+    }
+    log.Print(products)
+    context.JSON(http.StatusOK, products)    
+  })
+
   router.GET("/product/:id", func(context *gin.Context) {
     id, _ := strconv.Atoi(context.Param("id"))
     context.JSON(http.StatusOK, database.GetProduct(id))    
@@ -55,6 +75,23 @@ func serveApplication() {
     fmt.Println(product)
 	  database.AddProduct(product)
     context.JSON(http.StatusAccepted,&product)   
+  })
+
+  router.POST("/database_write_conn", func(context *gin.Context) {
+    conn, _ := pgx.Connect(ctx.Background(), os.Getenv("DB_URL"))
+	  defer conn.Close(ctx.Background())
+	  conn.Exec(ctx.Background(), "insert into product (product_name, product_price) values ('nice product', 22)")
+    context.JSON(http.StatusOK, "ok")    
+  })
+
+  router.POST("/database_write_many", func(context *gin.Context) {
+    for i := 0; i < 10; i++ {
+      product := model.Product{}
+      product.Name = "Sample name"
+      product.Price = int32(i)
+      database.AddProduct(product)
+    }
+    context.JSON(http.StatusOK, "Done")
   })
 
   router.Run(":8000") //running application, Default port is 8080
