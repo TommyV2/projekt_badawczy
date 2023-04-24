3649c320818aafef15bac67028d6c0d4db388df9
Z6FFK <ffk@kmd.dk>
2023-04-24 10:44:12 +0200

    New endpoints in Ruby on Rails.

--- ruby_on_rails/config/routes.rb
+++ ruby_on_rails/config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   get "/response", to: "response#index"
-  get "/database_read", to: "database#read"
   get "/product/:productId", to: "database#product_by_id"
+  get "/database_read", to: "database#read"
+  get "/database_read_conn", to: "database#read_conn"
   post "/database_write", to: "database#write"
+  post "/database_write_conn", to: "database#write_conn"
+  post "/database_write_many", to: "database#write_many"
+  post "/database_write_many_conn", to: "database#write_many_conn"
 end
